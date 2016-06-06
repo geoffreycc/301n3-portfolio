21f447f31abccff8d5dcdcf54c14290c8c457096
geoffreycc <gcox3d@gmail.com>
2016-06-06 00:12:45 -0700

    Added articleDisplay.js and put in method names with comments.

---
+++ JS/articleDisplay.js
@@ -0,0 +1,25 @@
+var articleDisplay = {};
+
+articleDisplay.pupulateFilters = function() {
+//Add categories and Author names to the options
+};
+
+articleDisplay.authorSort = function() {
+//Show only selected author's articles.
+};
+
+articleDisplay.categorySort = function() {
+//Show only selected category artiles
+};
+
+articleDisplay.topNavBar = function() {
+//Show only articles or only about section on click
+};
+
+articleDisplay.teaserControl = function() {
+//Change from partial to full view of article
+};
+
+$(document).ready(function() {
+//Call methods
+});
