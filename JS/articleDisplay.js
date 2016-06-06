4da4f7224b01749ff684870cb5bdb39c096d5568
geoffreycc <gcox3d@gmail.com>
2016-06-06 01:45:44 -0700

    Author filter populates with duplicates.

--- JS/articleDisplay.js
+++ JS/articleDisplay.js
@@ -1,19 +1,31 @@
 var articleDisplay = {};
 
-articleDisplay.pupulateFilters = function() {
-//Add categories and Author names to the options
+articleDisplay.populateFilters = function() {
+  var $newAuthor = $('.autSort').clone();
+  $('#projects > article').each(function() {
+    if($(this).hasClass('articleContent')) {
+      var newOption = '<option>' + $(this).find('h2').attr('data-author') + '</option>';
+      $('#author-sort').append(newOption);
+      // $('#author-sort').append($('.autSort').clone().text($(this).find($('nav p:first-child')).text())); //This is wonky
+      // $newAuthor.text($(this).find)('nav p:first-child').text();
+      // $newAuthor.removeClass('autSort');
+    };
+  });
 };
 
 articleDisplay.authorSort = function() {
 //Show only selected author's articles.
+  $('#author-sort');
 };
 
 articleDisplay.categorySort = function() {
 //Show only selected category artiles
+  $('#category-sort');
 };
 
 articleDisplay.topNavBar = function() {
 //Show only articles or only about section on click
+  $('header nav');
 };
 
 articleDisplay.teaserControl = function() {
