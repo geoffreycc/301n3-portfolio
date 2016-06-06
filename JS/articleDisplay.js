14810442e9046f38e62a0b96e47f8235b51e1715
geoffreycc <gcox3d@gmail.com>
2016-06-06 13:20:04 -0700

    Authors filter updated without duplicates.

--- JS/articleDisplay.js
+++ JS/articleDisplay.js
@@ -1,17 +1,37 @@
 var articleDisplay = {};
 
 articleDisplay.populateFilters = function() {
-  var $newAuthor = $('.autSort').clone();
   $('#projects > article').each(function() {
-    if($(this).hasClass('articleContent')) {
-      var newOption = '<option>' + $(this).find('h2').attr('data-author') + '</option>';
-      $('#author-sort').append(newOption);
-      // $('#author-sort').append($('.autSort').clone().text($(this).find($('nav p:first-child')).text())); //This is wonky
-      // $newAuthor.text($(this).find)('nav p:first-child').text();
-      // $newAuthor.removeClass('autSort');
-    };
+    if ($(this).hasClass('articleContent')) {
+      // var $newOption = $('#author-sort :first-child').clone();
+      // ($newOption).text($(this).find('h2').attr('data-author'));
+      var optionContent = ($(this).find('h2').attr('data-author'));
+      var newOptionTag = '<option value="' + optionContent + '">' + optionContent + '</option>';
+      console.log(newOptionTag);
+      if ($('#author-sort option[value="' + optionContent + '"]').length === 0) {
+        console.log('Okay');
+        $('#author-sort').append(newOptionTag);
+      }
+      // if (!$('#author-sort option:contains(' + ($newOption).text() + ')')) {
+      // // '[value ="' + $newOption + '"]').length < 1) {
+      //   $('#author-sort').append($newOption);
+      // }
+      // '<option>' + $(this).find('h2').attr('data-author') + '</option>'; from above line
+      // $('#author-sort option').each(function() {
+      //.length === 0
+      // });
+    }
   });
 };
+//         }
+//
+//       // $('#author-sort').append($('.autSort').clone().text($(this).find($('nav p:first-child')).text())); //This is wonky
+//       // $newAuthor.text($(this).find)('nav p:first-child').text();
+//       // $newAuthor.removeClass('autSort');
+//
+//   }
+// });
+// }}
 
 articleDisplay.authorSort = function() {
 //Show only selected author's articles.
