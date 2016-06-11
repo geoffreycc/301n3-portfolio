30b8f7c295a3694381af1e07ced9397daed26754
geoffreycc <gcox3d@gmail.com>
2016-06-10 22:18:21 -0700

    Made handlebars render method. Not working.

--- js/app.js
+++ js/app.js
@@ -13,21 +13,24 @@ function ArticleObj(art) {
 };
 
 ArticleObj.prototype.contentDisplay = function () {
-  var $section = $('.newArticle').clone();
-
-  $section.attr('data-category', this.category).attr('data-author', this.author);
-  $section.find('h2').text(this.title).attr('data-category', this.category).attr('data-author', this.author);
-  $section.find('img').attr('src', this.imgPath);
-  $section.find('p').html(this.about);
-  $section.find('a').attr('href', this.link).text(this.title + ' Repository');
-  $section.find('nav p:first-child').text('By ' + this.author);
-  $section.find('nav > p:last-of-type').text('Published ' + this.publishedOn);
+  // var $section = $('.newArticle').clone();
+  //
+  // $section.attr('data-category', this.category).attr('data-author', this.author);
+  // $section.find('h2').text(this.title).attr('data-category', this.category).attr('data-author', this.author);
+  // $section.find('img').attr('src', this.imgPath);
+  // $section.find('p').html(this.about);
+  // $section.find('a').attr('href', this.link).text(this.title + ' Repository');
+  // $section.find('nav p:first-child').text('By ' + this.author);
+  // $section.find('nav > p:last-of-type').text('Published ' + this.publishedOn);
   // $section.find('nav ')'Published ' + this.publishedOn);
 
-  $section.removeClass('newArticle');
-  $section.addClass('articleContent');
-
-  return $section;
+  // $section.removeClass('newArticle');
+  // $section.addClass('articleContent');
+  //
+  // return $section;
+  var templateIndex = $('#blogArticle').html();
+  var template = Handlebars.compile(templateIndex);
+  return template(this);
 };
 
 articles.forEach(function(ele){
