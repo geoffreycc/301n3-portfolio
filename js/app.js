f0ed0ff990d8a884935bbacd8c34954849009783
geoffreycc <gcox3d@gmail.com>
2016-06-11 13:20:09 -0700

    Removed some comments.

--- js/app.js
+++ js/app.js
@@ -13,28 +13,9 @@ function ArticleObj(art) {
 };
 
 ArticleObj.prototype.contentDisplay = function () {
-  // var $section = $('.newArticle').clone();
-  //
-  // $section.attr('data-category', this.category).attr('data-author', this.author);
-  // $section.find('h2').text(this.title).attr('data-category', this.category).attr('data-author', this.author);
-  // $section.find('img').attr('src', this.imgPath);
-  // $section.find('p').html(this.about);
-  // $section.find('a').attr('href', this.link).text(this.title + ' Repository');
-  // $section.find('nav p:first-child').text('By ' + this.author);
-  // $section.find('nav > p:last-of-type').text('Published ' + this.publishedOn);
-  // $section.find('nav ')'Published ' + this.publishedOn);
-
-  // $section.removeClass('newArticle');
-  // $section.addClass('articleContent');
-  //
-  // return $section;
-  // console.log('contentDisplay ran');
   var templateIndex = $('#blogArticle').html();
-  // console.log(templateIndex);
   var template = Handlebars.compile(templateIndex);
-  // console.log(template);
   return template(this);
-  // console.log(this + 'Here is this');
 };
 
 $(document).ready(function() {
