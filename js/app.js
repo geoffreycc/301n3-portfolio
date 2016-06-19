b32b0221e1b83f9e3bff5aa1868f044d0798a36c
geoffreycc <gcox3d@gmail.com>
2016-06-19 14:20:06 -0700

    Reworked .getJSON method and changed .json file.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-var articleContent = [];
 
 function ArticleObj(art) {
   this.title = art.title;
@@ -9,27 +8,72 @@ function ArticleObj(art) {
   this.publishedOn = art.publishedOn;
   this.category = art.category;
   this.author = art.author;
-  articleContent.push(this);
+  ArticleObj.all.push(this);
 };
 
-ArticleObj.prototype.contentDisplay = function () {
+ArticleObj.all = []; //was articleContent?
+
+ArticleObj.prototype.contentDisplay = function() {
   var templateIndex = $('#blogArticle').html();
   var template = Handlebars.compile(templateIndex);
   return template(this);
 };
 
-ArticleObj.prototype.setDates = function () {
+ArticleObj.prototype.setDates = function() {
   //Create method to make new dates
 };
 
+ArticleObj.checkLocal = function() {
+  // var rawData = [];
+  // if (localStorage.rawData) {
+  //   rawData = JSON.parse(localStorage.rawData); //
+  //   console.log('articleDisplay first if statement ran.');
+  //   //get localContent
+  // } else {
+  //   $.getJSON('/data/article.json', function(data) {
+  //     console.log(data);
+  //     localStorage.setItem('rawData', JSON.stringify(data));
+  //     console.log('articleDisplay else statement ran.');
+  //   }); //?
+  //
+  //   //create localContent
+  // }
+};
+
+ArticleObj.loadArticles = function(rawData) {
+  rawData.map(function(ele) {
+    console.log(ele);
+    return new ArticleObj(ele);
+  });
+};
+
+ArticleObj.getRawData = function() {
+  $.getJSON('data/article.json', function(rawData) {
+    ArticleObj.loadArticles(rawData);
+    localStorage.rawData = JSON.stringify(rawData);
+  });
+};
+
+ArticleObj.getRawData();
+
+//
+// articleDisplay.getFromServer = function () {
+//   //use AJAX to get content form json file
+// };
+//
+// articleDisplay.checkLatest = function() {
+//   //getAjax head deal?
+//   //Check to see if local storage is up to date.
+// };
+
 //Create a method to sort articles by date so that the most recent is presented first.
 
-$(document).ready(function() {
-  articles.forEach(function(ele){
-    var art = new ArticleObj(ele);
-  });
+// $(document).ready(function() {
+//   articles.forEach(function(ele){
+//     var art = new ArticleObj(ele);
+//   });
 
-  articleContent.forEach(function(ar) {
-    $('#projects').append(ar.contentDisplay());
-  });
-});
+//   articleContent.forEach(function(ar) {
+//     $('#projects').append(ar.contentDisplay());
+//   });
+// });
