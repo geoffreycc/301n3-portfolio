eb3fc0324934525c465f495f8c51466a8e86616f
geoffreycc <gcox3d@gmail.com>
2016-07-10 12:52:54 -0700

    Added some comments.

--- js/gitControl.js
+++ js/gitControl.js
@@ -6,9 +6,9 @@ gitControl.repos = [];
 
 gitControl.getRepo = function(callback) {
   $.ajax({
-    url: 'https://api.github.com/users/geoffreycc/repos',
+    url: 'https://api.github.com/users/geoffreycc/repos', //  Change url to match the one mentioned in class.
     type: 'GET',
-    header: {'Authorization': 'token ' + gitHubKey},
+    header: {'Authorization': 'token ' + gitHubKey}, // Remove this header? Call on something else here?
     success: function(data) {
       gitControl.repos = data;
       callback(); //Do something with the array of repo objects that are returned.
