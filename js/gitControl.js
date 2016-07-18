2539d53e28f762d2ad1af79b0db65cf42372b37f
geoffreycc <gcox3d@gmail.com>
2016-07-18 15:00:46 -0700

    Added process.env back

--- js/gitControl.js
+++ js/gitControl.js
@@ -6,7 +6,7 @@ gitControl.getRepo = function() {
   $.ajax({
     url: 'https://api.github.com/user/repos', //  Change url to match the one mentioned in class.  user/repos + api key
     type: 'GET',
-    headers: {'Authorization': 'token ' + GITHUBKEY}, // Remove this header? Call on something else here? process.env.GITHUBKEY
+    headers: {'Authorization': 'token ' + process.env.GITHUBKEY}, // Remove this header? Call on something else here? process.env.GITHUBKEY
     success: function(data) {
       gitControl.repos = data;
       // next(); //Do something with the array of repo objects that are returned.
