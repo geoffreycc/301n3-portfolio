245df7bbe18bf3c5b92bc27f74ff9c7f7e0d611b
geoffreycc <gcox3d@gmail.com>
2016-07-18 14:25:51 -0700

    removed quotes from authorization

--- js/gitControl.js
+++ js/gitControl.js
@@ -2,14 +2,14 @@ var gitControl = {};
 
 gitControl.repos = [];
 
-gitControl.getRepo = function(next) {
+gitControl.getRepo = function() {
   $.ajax({
     url: 'https://api.github.com/user/repos', //  Change url to match the one mentioned in class.  user/repos + api key
     type: 'GET',
-    header: {'Authorization': 'token ' + process.env.gitHubKey}, // Remove this header? Call on something else here? process.env.GITHUBKEY
+    header: {Authorization: 'token ' + process.env.gitHubKey}, // Remove this header? Call on something else here? process.env.GITHUBKEY
     success: function(data) {
       gitControl.repos = data;
-      next(); //Do something with the array of repo objects that are returned.
+      // next(); //Do something with the array of repo objects that are returned.
     }
   });
 };
